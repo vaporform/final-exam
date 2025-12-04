49cbdf4ad3a9bbb08e360b0ae0fbcfd3b811248b
vaporform <ysktyrch@gmail.com>
2025-12-04 15:23:25 +0700

    Finishing up #1

--- m1.py
+++ m1.py
@@ -3,7 +3,7 @@ class Person:
         self.name = name
     
     def introduce(self):
-        print(f"Hi, I'm {self.name}")
+        print(f"Hi, I'm {self.name}.")
     
 class Customer(Person):
     def __init__(self,name,address):
@@ -21,7 +21,7 @@ class Driver(Person):
     
     def deliver(self,order):
         # NTS: ORDER IS DELIVERY ORDER!
-        print(f"{self.name} is delivering {order.item} to {order.customer} using {self.vehicle}.")
+        print(f"{self.name} is delivering {order.item} to {order.customer.name} using {self.vehicle}.")
         order.status = "delivered"
     
 class DeliveryOrder:
@@ -29,12 +29,15 @@ class DeliveryOrder:
         self.customer = c
         self.item = i
         self.status = "preparing"
-    
+        self.driver = None #????
+
     def assign_driver(self,driver):
-        driver.deliver()
+        # Assuming that the assign driver creates a self.driver. 
+        # This is for accessing the driver in summary.
+        self.driver = driver
     
     def summary(self):
-        l = f'Item: {self.item}\nCustomer: {self.customer.name}\nStatus: {self.status}'
+        l = f'Item: {self.item}\nCustomer: {self.customer.name}\nStatus: {self.status}\nDriver: {self.driver.name}'
         return l
     
 # MAIN
@@ -47,9 +50,18 @@ d.introduce()
 print()
 do1 = DeliveryOrder(c1,'Laptop')
 do2 = DeliveryOrder(c2,'Headphones') 
-
-# DONT FORGET TO CHECK THE DO1 DO2!
+do1.assign_driver(d)
+do2.assign_driver(d)
+# DONT FORGET TO CHECK THE DO1 DO2! ##NY DRIVER!
+print("Order Summary:")
 print(do1.summary())
 print()
+print("Order Summary:")
 print(do2.summary())
-
+print()
+d.deliver(do1)
+d.deliver(do2)
+print()
+print("Final Status:")
+print(f"Order for {do1.item} → {do1.status}")
+print(f"Order for {do2.item} → {do2.status}")
